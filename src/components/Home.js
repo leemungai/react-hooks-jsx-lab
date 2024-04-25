a1028ba53c8e906c10bfb884d4a41f4c6fe8a622
leemungai <mrmungai@yahoo.com>
2024-04-25 18:42:03 +0200

    Complete jsx-lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+function Content() {
+  return (
+    <h1 style={{ color: "firebrick" }}>
+      {name} is a Web Developer from {city}
+    </h1>
+  );
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <Content />
+    </div>
+  );
 }
 
 export default Home;
